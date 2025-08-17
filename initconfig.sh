4e36f400af1207b932b6ccf8da2d8faef412918a
HappDev <114048814+HappDev@users.noreply.github.com>
2025-08-17 20:18:56 +0800

    Update initconfig.sh

--- initconfig.sh
+++ initconfig.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # One-click configuration
 
-# Check if system has IPv6 address
+# Check if the system has an IPv6 address
 check_ipv6_support() {
     if ip -6 addr | grep -q "inet6"; then
-        echo "1"  # IPv6 supported
+        echo "1"  # Supports IPv6
     else
-        echo "0"  # IPv6 not supported
+        echo "0"  # Does not support IPv6
     fi
 }
 
@@ -15,7 +15,7 @@ add_node_config() {
     echo -e "${green}1. xray${plain}"
     echo -e "${green}2. singbox${plain}"
     echo -e "${green}3. hysteria2${plain}"
-    read -rp "Enter: " core_type
+    read -rp "Enter your choice: " core_type
     if [ "$core_type" == "1" ]; then
         core="xray"
         core_xray=true
@@ -26,16 +26,16 @@ add_node_config() {
         core="hysteria2"
         core_hysteria2=true
     else
-        echo "Invalid choice. Please select 1 2 3."
+        echo "Invalid choice. Please select 1, 2, or 3."
         continue
     fi
     while true; do
-        read -rp "Enter Node ID: " NodeID
+        read -rp "Enter the Node ID: " NodeID
         # Check if NodeID is a positive integer
         if [[ "$NodeID" =~ ^[0-9]+$ ]]; then
-            break  # correct input, exit loop
+            break  # Valid input, exit loop
         else
-            echo "Error: Please enter a valid number as Node ID."
+            echo "Error: Please enter a valid number for the Node ID."
         fi
     done
 
@@ -58,7 +58,7 @@ add_node_config() {
             echo -e "${green}7. Tuic${plain}"
             echo -e "${green}8. AnyTLS${plain}"
         fi
-        read -rp "Enter: " NodeType
+        read -rp "Enter your choice: " NodeType
         case "$NodeType" in
             1 ) NodeType="shadowsocks" ;;
             2 ) NodeType="vless" ;;
@@ -73,32 +73,32 @@ add_node_config() {
     fi
     fastopen=true
     if [ "$NodeType" == "vless" ]; then
-        read -rp "Is this a reality node? (y/n): " isreality
+        read -rp "Is this a reality node? (y/n) " isreality
     elif [ "$NodeType" == "hysteria" ] || [ "$NodeType" == "hysteria2" ] || [ "$NodeType" == "tuic" ] || [ "$NodeType" == "anytls" ]; then
         fastopen=false
         istls="y"
     fi
 
     if [[ "$isreality" != "y" && "$isreality" != "Y" &&  "$istls" != "y" ]]; then
-        read -rp "Do you want to configure TLS? (y/n): " istls
+        read -rp "Would you like to configure TLS? (y/n) " istls
     fi
 
     certmode="none"
     certdomain="example.com"
     if [[ "$isreality" != "y" && "$isreality" != "Y" && ( "$istls" == "y" || "$istls" == "Y" ) ]]; then
-        echo -e "${yellow}Please select certificate mode:${plain}"
-        echo -e "${green}1. http mode (auto issue, domain already resolved)${plain}"
-        echo -e "${green}2. dns mode (auto issue, requires correct DNS provider API parameters)${plain}"
-        echo -e "${green}3. self mode (self-signed certificate or existing certificate)${plain}"
-        read -rp "Enter: " certmode
+        echo -e "${yellow}Please select the certificate application mode:${plain}"
+        echo -e "${green}1. HTTP mode (automatic application, node domain must be correctly resolved)${plain}"
+        echo -e "${green}2. DNS mode (automatic application, requires correct DNS provider API parameters)${plain}"
+        echo -e "${green}3. Self mode (self-signed certificate or provide existing certificate files)${plain}"
+        read -rp "Enter your choice: " certmode
         case "$certmode" in
             1 ) certmode="http" ;;
             2 ) certmode="dns" ;;
             3 ) certmode="self" ;;
         esac
-        read -rp "Enter certificate domain (example.com): " certdomain
+        read -rp "Enter the node certificate domain (example.com): " certdomain
         if [ "$certmode" != "http" ]; then
-            echo -e "${red}Please manually edit the config file and restart V2bX!${plain}"
+            echo -e "${red}Please manually modify the configuration file and restart V2bX!${plain}"
         fi
     fi
     ipv6_support=$(check_ipv6_support)
@@ -111,7 +111,7 @@ add_node_config() {
     node_config=$(cat <<EOF
 {
             "Core": "$core",
-            "ApiHost": "dev.ffr.su",
+            "ApiHost": "$ApiHost",
             "ApiKey": "$ApiKey",
             "NodeID": $NodeID,
             "NodeType": "$NodeType",
@@ -143,7 +143,7 @@ EOF
     node_config=$(cat <<EOF
 {
             "Core": "$core",
-            "ApiHost": "dev.ffr.su",
+            "ApiHost": "$ApiHost",
             "ApiKey": "$ApiKey",
             "NodeID": $NodeID,
             "NodeType": "$NodeType",
@@ -173,7 +173,7 @@ EOF
     node_config=$(cat <<EOF
 {
             "Core": "$core",
-            "ApiHost": "dev.ffr.su",
+            "ApiHost": "$ApiHost",
             "ApiKey": "$ApiKey",
             "NodeID": $NodeID,
             "NodeType": "$NodeType",
@@ -203,14 +203,14 @@ EOF
 }
 
 generate_config_file() {
-    echo -e "${yellow}V2bX Config File Wizard${plain}"
+    echo -e "${yellow}V2bX Configuration File Generation Wizard${plain}"
     echo -e "${red}Please read the following notes:${plain}"
-    echo -e "${red}1. This feature is currently in testing phase${plain}"
-    echo -e "${red}2. The generated config will be saved to /etc/V2bX/config.json${plain}"
-    echo -e "${red}3. The old config will be saved as /etc/V2bX/config.json.bak${plain}"
-    echo -e "${red}4. Currently only partial TLS support${plain}"
-    echo -e "${red}5. The generated config includes audit. Continue? (y/n)${plain}"
-    read -rp "Enter: " continue_prompt
+    echo -e "${red}1. This feature is currently in the testing phase${plain}"
+    echo -e "${red}2. The generated configuration file will be saved to /etc/V2bX/config.json${plain}"
+    echo -e "${red}3. The original configuration file will be backed up to /etc/V2bX/config.json.bak${plain}"
+    echo -e "${red}4. TLS support is currently limited${plain}"
+    echo -e "${red}5. The generated configuration file includes auditing. Proceed? (y/n)${plain}"
+    read -rp "Enter your choice: " continue_prompt
     if [[ "$continue_prompt" =~ ^[Nn][Oo]? ]]; then
         exit 0
     fi
@@ -220,35 +220,36 @@ generate_config_file() {
     core_xray=false
     core_sing=false
     core_hysteria2=false
-    fixed_api_info=true
+    fixed_api_info=false
     check_api=false
     
     while true; do
         if [ "$first_node" = true ]; then
-            read -rp "Enter site URL (https://example.com): " ApiHost
-            read -rp "Enter panel API Key: " ApiKey
-#//            read -rp "Fix the site URL and API Key? (y/n): " fixed_api
+            read -rp "Enter the server URL[](https://example.com): " ApiHost
+            read -rp "Enter the panel API Key: " ApiKey
+            read -rp "Would you like to set a fixed server URL and API Key? (y/n) " fixed_api
             if [ "$fixed_api" = "y" ] || [ "$fixed_api" = "Y" ]; then
                 fixed_api_info=true
-                echo -e "${red}Fixed successfully${plain}"
+                echo -e "${red}Successfully fixed the address${plain}"
             fi
             first_node=false
             add_node_config
         else
-            read -rp "Add another node? (Enter to continue, n or no to exit): " continue_adding_node
+            read -rp "Would you like to add another node configuration? (Press Enter to continue, or type 'n' or 'no' to exit) " continue_adding_node
             if [[ "$continue_adding_node" =~ ^[Nn][Oo]? ]]; then
                 break
             elif [ "$fixed_api_info" = false ]; then
-                read -rp "Enter site URL (https://example.com): " ApiHost
-                read -rp "Enter panel API Key: " ApiKey
+                read -rp "Enter the server URL[](https://example.com): " ApiHost
+                read -rp "Enter the panel API Key: " ApiKey
             fi
             add_node_config
         fi
     done
 
-    # Build core config array
+    # Initialize core configuration array
     cores_config="["
 
+    # Check and add xray core configuration
     if [ "$core_xray" = true ]; then
         cores_config+="
     {
@@ -262,6 +263,7 @@ generate_config_file() {
     },"
     fi
 
+    # Check and add sing core configuration
     if [ "$core_sing" = true ]; then
         cores_config+="
     {
@@ -279,6 +281,7 @@ generate_config_file() {
     },"
     fi
 
+    # Check and add hysteria2 core configuration
     if [ "$core_hysteria2" = true ]; then
         cores_config+="
     {
@@ -289,15 +292,19 @@ generate_config_file() {
     },"
     fi
 
+    # Remove the last comma and close the array
     cores_config+="]"
     cores_config=$(echo "$cores_config" | sed 's/},]$/}]/')
 
+    # Switch to the configuration file directory
     cd /etc/V2bX
     
+    # Backup the old configuration file
     mv config.json config.json.bak
     nodes_config_str="${nodes_config[*]}"
     formatted_nodes_config="${nodes_config_str%,}"
 
+    # Create config.json file
     cat <<EOF > /etc/V2bX/config.json
 {
     "Log": {
@@ -309,6 +316,7 @@ generate_config_file() {
 }
 EOF
     
+    # Create custom_outbound.json file
     cat <<EOF > /etc/V2bX/custom_outbound.json
 [
     {
@@ -332,6 +340,7 @@ EOF
 ]
 EOF
     
+    # Create route.json file
     cat <<EOF > /etc/V2bX/route.json
 {
     "domainStrategy": "AsIs",
@@ -342,17 +351,62 @@ EOF
                 "geoip:private"
             ]
         },
-        ...
+        {
+            "outboundTag": "block",
+            "domain": [
+                "regexp:(api|ps|sv|offnavi|newvector|ulog.imap|newloc)(.map|).(baidu|n.shifen).com",
+                "regexp:(.+.|^)(360|so).(cn|com)",
+                "regexp:(Subject|HELO|SMTP)",
+                "regexp:(torrent|.torrent|peer_id=|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=)",
+                "regexp:(^.@)(guerrillamail|guerrillamailblock|sharklasers|grr|pokemail|spam4|bccto|chacuo|027168).(info|biz|com|de|net|org|me|la)",
+                "regexp:(.?)(xunlei|sandai|Thunder|XLLiveUD)(.)",
+                "regexp:(..||)(dafahao|mingjinglive|botanwang|minghui|dongtaiwang|falunaz|epochtimes|ntdtv|falundafa|falungong|wujieliulan|zhengjian).(org|com|net)",
+                "regexp:(ed2k|.torrent|peer_id=|announce|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=|magnet:|xunlei|sandai|Thunder|XLLiveUD|bt_key)",
+                "regexp:(.+.|^)(360).(cn|com|net)",
+                "regexp:(.*.||)(guanjia.qq.com|qqpcmgr|QQPCMGR)",
+                "regexp:(.*.||)(rising|kingsoft|duba|xindubawukong|jinshanduba).(com|net|org)",
+                "regexp:(.*.||)(netvigator|torproject).(com|cn|net|org)",
+                "regexp:(..||)(visa|mycard|gash|beanfun|bank).",
+                "regexp:(.*.||)(gov|12377|12315|talk.news.pts.org|creaders|zhuichaguoji|efcc.org|cyberpolice|aboluowang|tuidang|epochtimes|zhengjian|110.qq|mingjingnews|inmediahk|xinsheng|breakgfw|chengmingmag|jinpianwang|qi-gong|mhradio|edoors|renminbao|soundofhope|xizang-zhiye|bannedbook|ntdtv|12321|secretchina|dajiyuan|boxun|chinadigitaltimes|dwnews|huaglad|oneplusnews|epochweekly|cn.rfi).(cn|com|org|net|club|net|fr|tw|hk|eu|info|me)",
+                "regexp:(.*.||)(miaozhen|cnzz|talkingdata|umeng).(cn|com)",
+                "regexp:(.*.||)(mycard).(com|tw)",
+                "regexp:(.*.||)(gash).(com|tw)",
+                "regexp:(.bank.)",
+                "regexp:(.*.||)(pincong).(rocks)",
+                "regexp:(.*.||)(taobao).(com)",
+                "regexp:(.*.||)(laomoe|jiyou|ssss|lolicp|vv1234|0z|4321q|868123|ksweb|mm126).(com|cloud|fun|cn|gs|xyz|cc)",
+                "regexp:(flows|miaoko).(pages).(dev)"
+            ]
+        },
+        {
+            "outboundTag": "block",
+            "ip": [
+                "127.0.0.1/32",
+                "10.0.0.0/8",
+                "fc00::/7",
+                "fe80::/10",
+                "172.16.0.0/12"
+            ]
+        },
+        {
+            "outboundTag": "block",
+            "protocol": [
+                "bittorrent"
+            ]
+        },
+        {
+            "outboundTag": "IPv4_out",
+            "network": "udp,tcp"
+        }
     ]
 }
 EOF
-
     ipv6_support=$(check_ipv6_support)
     dnsstrategy="ipv4_only"
     if [ "$ipv6_support" -eq 1 ]; then
         dnsstrategy="prefer_ipv4"
     fi
-
+    # Create sing_origin.json file
     cat <<EOF > /etc/V2bX/sing_origin.json
 {
   "dns": {
@@ -364,15 +418,92 @@ EOF
     ],
     "strategy": "$dnsstrategy"
   },
-  ...
+  "outbounds": [
+    {
+      "tag": "direct",
+      "type": "direct",
+      "domain_resolver": {
+        "server": "cf",
+        "strategy": "$dnsstrategy"
+      }
+    },
+    {
+      "type": "block",
+      "tag": "block"
+    }
+  ],
+  "route": {
+    "rules": [
+      {
+        "ip_is_private": true,
+        "outbound": "block"
+      },
+      {
+        "domain_regex": [
+            "(api|ps|sv|offnavi|newvector|ulog.imap|newloc)(.map|).(baidu|n.shifen).com",
+            "(.+.|^)(360|so).(cn|com)",
+            "(Subject|HELO|SMTP)",
+            "(torrent|.torrent|peer_id=|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=)",
+            "(^.@)(guerrillamail|guerrillamailblock|sharklasers|grr|pokemail|spam4|bccto|chacuo|027168).(info|biz|com|de|net|org|me|la)",
+            "(.?)(xunlei|sandai|Thunder|XLLiveUD)(.)",
+            "(..||)(dafahao|mingjinglive|botanwang|minghui|dongtaiwang|falunaz|epochtimes|ntdtv|falundafa|falungong|wujieliulan|zhengjian).(org|com|net)",
+            "(ed2k|.torrent|peer_id=|announce|info_hash|get_peers|find_node|BitTorrent|announce_peer|announce.php?passkey=|magnet:|xunlei|sandai|Thunder|XLLiveUD|bt_key)",
+            "(.+.|^)(360).(cn|com|net)",
+            "(.*.||)(guanjia.qq.com|qqpcmgr|QQPCMGR)",
+            "(.*.||)(rising|kingsoft|duba|xindubawukong|jinshanduba).(com|net|org)",
+            "(.*.||)(netvigator|torproject).(com|cn|net|org)",
+            "(..||)(visa|mycard|gash|beanfun|bank).",
+            "(.*.||)(gov|12377|12315|talk.news.pts.org|creaders|zhuichaguoji|efcc.org|cyberpolice|aboluowang|tuidang|epochtimes|zhengjian|110.qq|mingjingnews|inmediahk|xinsheng|breakgfw|chengmingmag|jinpianwang|qi-gong|mhradio|edoors|renminbao|soundofhope|xizang-zhiye|bannedbook|ntdtv|12321|secretchina|dajiyuan|boxun|chinadigitaltimes|dwnews|huaglad|oneplusnews|epochweekly|cn.rfi).(cn|com|org|net|club|net|fr|tw|hk|eu|info|me)",
+            "(.*.||)(miaozhen|cnzz|talkingdata|umeng).(cn|com)",
+            "(.*.||)(mycard).(com|tw)",
+            "(.*.||)(gash).(com|tw)",
+            "(.bank.)",
+            "(.*.||)(pincong).(rocks)",
+            "(.*.||)(taobao).(com)",
+            "(.*.||)(laomoe|jiyou|ssss|lolicp|vv1234|0z|4321q|868123|ksweb|mm126).(com|cloud|fun|cn|gs|xyz|cc)",
+            "(flows|miaoko).(pages).(dev)"
+        ],
+        "outbound": "block"
+      },
+      {
+        "outbound": "direct",
+        "network": [
+          "udp","tcp"
+        ]
+      }
+    ]
+  },
+  "experimental": {
+    "cache_file": {
+      "enabled": true
+    }
+  }
 }
 EOF
 
+    # Create hy2config.yaml file           
     cat <<EOF > /etc/V2bX/hy2config.yaml
 quic:
   initStreamReceiveWindow: 8388608
-  ...
+  maxStreamReceiveWindow: 8388608
+  initConnReceiveWindow: 20971520
+  maxConnReceiveWindow: 20971520
+  maxIdleTimeout: 30s
+  maxIncomingStreams: 1024
+  disablePathMTUDiscovery: false
+ignoreClientBandwidth: false
+disableUDP: false
+udpIdleTimeout: 60s
+resolver:
+  type: system
+acl:
+  inline:
+    - direct(geosite:google)
+    - reject(geosite:cn)
+    - reject(geoip:cn)
+masquerade:
+  type: 404
 EOF
-    echo -e "${green}V2bX configuration file generated successfully, restarting service${plain}"
+    echo -e "${green}V2bX configuration file generation completed, restarting the service${plain}"
     v2bx restart
 }
